d86561c0ff9277dc3553a917c87fa57272553113
Josh J <33074635+JoshJ96@users.noreply.github.com>
2021-12-14 10:05:57 -0500

    Task initial definition

--- API/API/Models/Task.cs
+++ API/API/Models/Task.cs
@@ -8,6 +8,14 @@ namespace API.Models
 {
     internal class Task
     {
-
+        public string? name { get; set; }
+        public string? description { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public bool isCompleted { get; set; }
+        public int MyProperty { get; set; }
+        public List<Task>? SubTasks { get; set; }
+        public List<Task>? dependsOn { get; set; }
+        public List<Task>? dependedBy { get; set; }
     }
 }
