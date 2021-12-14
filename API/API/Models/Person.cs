786d4d25e399c6e1b45784473ddd0f412a304750
Josh J <33074635+JoshJ96@users.noreply.github.com>
2021-12-14 18:47:28 -0500

    Create Person.cs model

---
+++ API/API/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    internal class Person
+    {
+        public string? Name { get; set; }
+
+        public List<Task>? AssignedTasks { get; set; }
+    }
+}
